cf89c37476195237c1ac4fd025638c6b8483c877
avatarrrrr <marcosrios@pm.me>
2023-06-07 02:34:10 -0300

    [feat] class 1

---
+++ app/src/main/java/br/com/alura/aluratravels/ui/activity/PackagesListActivity.java
@@ -0,0 +1,16 @@
+package br.com.alura.aluratravels.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.alura.aluratravels.R;
+
+public class PackagesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_packages_list);
+    }
+}
